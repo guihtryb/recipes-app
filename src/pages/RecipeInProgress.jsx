8abdb76756e923636a802738ebed44f738029674
Guilherme Viana <guilhermehviana01@gmail.com>
2021-11-05 18:59:10 -0300

    Remove functions' props

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-function RecipeInProgress(props) {
+function RecipeInProgress() {
   return (
     <div />
   );
