a6851184a6db503cbecbdf17ac0179d2a49688e5
Caio César Amorim <caiocavalcante063@gmail.com>
2021-11-05 17:39:05 -0300

    fix eslint errors

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-function Details(props) {
+function Details() {
   return (
     <div>
       f
--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-function ExploreDrink(props) {
+function ExploreDrink() {
   return (
     <div />
   );
