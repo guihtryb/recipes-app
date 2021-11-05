0b37746a4ed411dcaea47ab46afaffc6e1d70f14
Caio César Amorim <caiocavalcante063@gmail.com>
2021-11-05 17:30:02 -0300

    cumpre requisito 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validateEmail, setValidateEmail] = useState(false);
+
+  // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+  const emailValidation = (userEmail) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(String(userEmail).toLowerCase());
+  };
+
+  const handleChange = ({ target: { value, id } }) => {
+    switch (id) {
+    case 'email-input':
+      setEmail(value);
+      setValidateEmail(emailValidation(value));
+      break;
+    case 'password-input':
+      setPassword(value);
+      break;
+    default:
+      return 0;
+    }
+    console.log(validateEmail);
+  };
+
+  const MIN_PASSWORD_CHARACTERS = 6;
+
   return (
     <>
       <input
@@ -8,17 +35,23 @@ function Login() {
         name="email-input"
         id="email-input"
         data-testid="email-input"
+        onChange={ handleChange }
+        value={ email }
       />
       <input
         type="password"
         name="password-input"
         id="password-input"
         data-testid="password-input"
+        onChange={ handleChange }
+        value={ password }
       />
       <button
         type="button"
         value="Entrar"
         data-testid="login-submit-btn"
+        disabled={ !(password.length > MIN_PASSWORD_CHARACTERS
+          && validateEmail) }
       >
         Entrar
       </button>
