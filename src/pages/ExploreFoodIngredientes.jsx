c9b92cdb18e537409799b51bd7ba0f2e2a2e7fcd
MartinSilva <martinempreendedor@gmail.com>
2021-11-05 19:29:35 -0300

    lint

--- src/pages/ExploreFoodIngredientes.jsx
+++ src/pages/ExploreFoodIngredientes.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
-
 
 function ExploreFoodIngredientes() {
   return (
