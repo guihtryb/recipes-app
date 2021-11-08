b3e8ffb41275ffefda415879fad0b0fbb6240df5
leodiber <leonardodiber@gmail.com>
2021-11-08 17:03:31 -0300

    Requisitos 70, 71 e 72

--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import LinkButton from '../components/LinkButton';
 // import PropTypes from 'prop-types';
 
 function ExploreDrink() {
   return (
     <div>
       <Header title="Explorar Bebidas" />
+      <div>
+        <LinkButton
+          text="Por Ingredientes"
+          testid="explore-by-ingredient"
+          linkTo="/explorar/bebidas/ingredientes"
+        />
+
+        <LinkButton
+          text="Me Surpreenda!"
+          testid="explore-surprise"
+          linkTo="/explorar/bebidas/ingredientes"
+        />
+
+      </div>
       <Footer />
     </div>
   );
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import LinkButton from '../components/LinkButton';
 // import PropTypes from 'prop-types';
 
 function ExploreFood() {
   return (
     <div>
       <Header title="Explorar Comidas" />
+      <div>
+        <LinkButton
+          text="Por Ingredientes"
+          testid="explore-by-ingredient"
+          linkTo="/explorar/comidas/ingredientes"
+        />
+        <LinkButton
+          text="Por Local de Origem"
+          testid="explore-by-area"
+          linkTo="/explorar/comidas/ingredientes"
+        />
+        <LinkButton
+          text="Me Surpreenda!"
+          testid="explore-surprise"
+          linkTo="/explorar/comidas/ingredientes"
+        />
+      </div>
       <Footer />
     </div>
   );
