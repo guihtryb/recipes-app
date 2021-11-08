5f1c437f3317d1b2a0c0bd22bc6ba173a7d2c787
Caio César Amorim <caiocavalcante063@gmail.com>
2021-11-08 15:44:40 -0300

    cumpre requisitos 82 a 84

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -4,9 +4,14 @@ import Footer from '../components/Footer';
 // import PropTypes from 'prop-types';
 
 function Perfil() {
+  const userEmail = JSON.parse(localStorage.getItem('user')).email;
   return (
     <div>
       <Header title="Perfil" />
+      <h2 data-testid="profile-email">{ userEmail }</h2>
+      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      <button type="button" data-testid="profile-logout-btn">Sair</button>
       <Footer />
     </div>
   );
