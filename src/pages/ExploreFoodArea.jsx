57f687c490dd96bc39c0707cf93711a9843f310d
Guilherme Viana <guilhermehviana01@gmail.com>
2021-11-09 20:56:14 -0300

    Req 79 - add function that search new recipes by area

--- src/pages/ExploreFoodArea.jsx
+++ src/pages/ExploreFoodArea.jsx
@@ -3,6 +3,8 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Context from '../context/Context';
 import RecipeCard from '../components/RecipeCard';
+import { fetchFoodReq } from '../services/APIs';
+
 // import PropTypes from 'prop-types';
 
 function ExploreFoodArea() {
@@ -13,16 +15,21 @@ function ExploreFoodArea() {
     && foodData[3].items.meals.slice(0, maxLength);
 
   const area = areaData ? areaData.items.meals : null;
+  const searchArea = async (searchedArea) => {
+    const newSearchData = await fetchFoodReq('filter', 'a', searchedArea);
+    console.log(newSearchData);
+  };
+
   if (!area) return <img src="https://media0.giphy.com/media/3o7bu8sRnYpTOG1p8k/giphy.gif?cid=ecf05e4739n2hlkxm6a8ymnheguv7bxk3f5m6wag9ocwigzy&rid=giphy.gif&ct=g" alt="" />;
   return (
     <div>
       <Header title="Explorar Origem" />
-      <select className="area-dropdown" data-testid="explore-by-area-dropdown">
+      <select className="area-dropdown" data-testid="explore-by-area-dropdown" onChange={ ({ target }) => searchArea(target.value) }>
         { area.map((item) => (
           <option
             data-testid={ `${item.strArea}-option` }
             key={ item.strArea }
-            value={ area.strArea }
+            value={ item.strArea }
           >
             { `${item.strArea}` }
           </option>
