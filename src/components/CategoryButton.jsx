acb6a74992846ac4b856715c1dbd8ad527ec0d2b
Caio César Amorim <caiocavalcante063@gmail.com>
2021-11-09 17:22:36 -0300

    cumpre requisitos 29 e 30

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { fetchFoodReq, fetchDrinkReq } from '../services/APIs';
 import Context from '../context/Context';
@@ -7,16 +7,24 @@ import Context from '../context/Context';
 function CategoryButton(props) {
   const { category } = props;
   const { setSearchData } = useContext(Context);
+  const [currentCategory, setCurrentCategory] = useState('');
 
   // https://stackoverflow.com/questions/42253277/react-router-v4-how-to-get-current-route
   const location = useLocation();
 
   const handleClick = async ({ target: { value } }) => {
-    const filteredByCategoryRecipes = location.pathname === '/comidas'
-      ? await fetchFoodReq('filter', 'c', value)
-      : await fetchDrinkReq('filter', 'c', value);
+    // se o botão para selecionar a categoria for clicado 2x seguidas, searchData é limpado
 
-    setSearchData(filteredByCategoryRecipes);
+    if (currentCategory === value) {
+      setSearchData([]);
+    } else {
+      const filteredByCategoryRecipes = location.pathname === '/comidas'
+        ? await fetchFoodReq('filter', 'c', value)
+        : await fetchDrinkReq('filter', 'c', value);
+
+      setSearchData(filteredByCategoryRecipes);
+      setCurrentCategory(value);
+    }
   };
 
   return (
