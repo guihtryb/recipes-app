235e3110a414d171e890028c703cef49427047d9
Guilherme Viana <guilhermehviana01@gmail.com>
2021-11-10 18:52:39 -0300

    Req 33 - recover id correctly

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-// import { fo }
+import { useLocation } from 'react-router';
 
 function Details() {
-  // resgatar id
+  const location = useLocation();
+  const path = location.pathname;
+  const id = path.split('s/')[1];
   // criar função para o clique no card
   // passar id para a função
   // verificar tipo de comida (bebidas ou refeições)
   // refeições ? www.themealdb.com/api/json/v1/1/lookup.php?i=52772 : www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007;
   // setar detailsData
+  // assim que o component montar
   //  work with data
   return (
     <div>
