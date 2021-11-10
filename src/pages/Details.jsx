3366b8cec5d26022ef15e6a90d168343d11948a0
Guilherme Viana <guilhermehviana01@gmail.com>
2021-11-10 18:49:20 -0300

    Init req 33

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+// import { fo }
 
 function Details() {
+  // resgatar id
+  // criar função para o clique no card
+  // passar id para a função
+  // verificar tipo de comida (bebidas ou refeições)
+  // refeições ? www.themealdb.com/api/json/v1/1/lookup.php?i=52772 : www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007;
+  // setar detailsData
+  //  work with data
   return (
     <div>
       Say hi
