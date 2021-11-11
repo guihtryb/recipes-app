3e9d2732c9736adc891d619f977b1abe72fc2666
Guilherme Viana <guilhermehviana01@gmail.com>
2021-11-11 07:10:17 -0300

    Setting details data to Page state

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,24 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import { fetchDrinkReq, fetchFoodReq } from '../services/APIs';
 
 function Details() {
+  const [detailsData, setDetailsData] = useState([]);
   const location = useLocation();
   const path = location.pathname;
   const id = path.split('s/')[1];
-  const type = path.includes('comidas') ? 'comidas' : 'bebidas';
-  const requisition = type === 'comidas' ? fetchFoodReq : fetchDrinkReq;
+  const type = path.includes('comidas') ? 'meals' : 'drinks';
+  const requisition = type === 'meals' ? fetchFoodReq : fetchDrinkReq;
 
   useEffect(() => {
     const getRecipeDetails = async () => {
-      const recipeDetails = await requisition('lookup', 'i', id);
+      const response = await requisition('lookup', 'i', id);
+      const recipeDetails = response[type];
+      setDetailsData(recipeDetails);
     };
     getRecipeDetails();
-  }, [requisition, id]);
-  // criar função para a manotagem do componente
-  // passar id para a função
-  // verificar tipo de comida (bebidas ou refeições)
-  // refeições ? www.themealdb.com/api/json/v1/1/lookup.php?i=52772 : www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007;
+  }, [type, requisition, id]);
+
   // setar detailsData
   // assim que o component montar
   //  work with data
