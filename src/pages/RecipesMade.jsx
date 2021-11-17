1caf13f499735b92377c04132d3cdf5b8c3e48ca
MartinSilva <martinempreendedor@gmail.com>
2021-11-17 01:07:23 -0300

    req 54

--- src/pages/RecipesMade.jsx
+++ src/pages/RecipesMade.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 import Header from '../components/Header';
 // import PropTypes from 'prop-types';
 
+/* [{
+  id: id-da-receita,
+  type: comida-ou-bebida,
+  area: area-da-receita-ou-texto-vazio,
+  category: categoria-da-receita-ou-texto-vazio,
+  alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
+  name: nome-da-receita,
+  image: imagem-da-receita
+}] */
+
+/*
+                objReceita.tags.reduce(
+                  (acc, tags) => (` ${acc}`),
+                  '')  */
+
 function RecipesMade() {
+  const tavaNoLocalStored = JSON.parse(localStorage.getItem('doneRecipes'));
+  console.log(tavaNoLocalStored);
   return (
     <>
       <Header title="Receitas Feitas" />
@@ -34,6 +51,50 @@ function RecipesMade() {
           </button>
         </div>
         <div>
+
+          { tavaNoLocalStored.map((objReceita, index) => (
+            <div
+              key={ objReceita.id }
+            >
+              {objReceita.tags.map((tags, indexx) => (
+                <div
+                  data-testid={ `${index}-${tags}-horizontal-tag` }
+                  key={ `${indexx} ${objReceita.id}` }
+
+                />))}
+              <img
+                className="details-image"
+                src={ objReceita.image }
+                alt=""
+                data-testid={ `${index}-horizontal-image` }
+                swidth="320"
+                height="205"
+
+              />
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {objReceita.category}
+              </p>
+              <p
+                data-testid={ `${index}-horizontal-name` }
+              >
+                {objReceita.name}
+              </p>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {objReceita.doneDate}
+              </p>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+              >
+                compartilha
+              </button>
+
+            </div>))}
+
           Gerador de card das receitas feitas
         </div>
       </div>
